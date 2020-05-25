cd741021ae0fd0247289d888e297fd9020a80c06
Hung Ly <hunghly@gmail.com>
2020-05-25 15:52:25 -0500

    working on fixing issue with event handling

--- src/index.js
+++ src/index.js
@@ -49,54 +49,77 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+import Moment from "moment";
 
-function Tweet() {
+function Tweet({tweet}) {
     return (
         <div className={"tweet"}>
-            <Avatar/>
+            <Avatar hash={tweet.gravatar}/>
             <div className="content">
-                <NameWithHandle/>
-                <Message/>
+                <NameWithHandle author={tweet.author}/>
+                <Time time={tweet.timestamp}/>
+                <Message text={tweet.message}/>
                 <div className="buttons">
-                    <LikeButton/>
+                    <LikeButton count={tweet.likes}/>
                     <CommentButton/>
                     <ShareButton/>
-                    <RetweetButton/>
+                    <RetweetButton count={tweet.retweets}/>
                 </div>
             </div>
         </div>
     )
 }
 
-function Avatar() {
+let testTweet = {
+    message: "Something about cats is the message",
+    gravatar: "xyz",
+    author: {
+        handle: 'catperson',
+        name: 'MorganWebperson'
+    },
+    likes: 2,
+    retweets: 0,
+    timestamp: '2019-09-25 21:23:37'
+}
+
+function Avatar({hash}) {
+    let url = `https://www.gravatar.com/avatar/${hash}`
     return (
-        <img src={"https://www.gravatar.com/avatar/nothing"} className={"avatar"} alt={'avatar'}/>
+        <img src={url} className={"avatar"} alt={'avatar'}/>
     )
 }
 
-function Message() {
+function Message({text}) {
     return (
         <div className={'message'}>
-            This is less than 140 characters.
+            {text}
         </div>
     )
 }
 
-function NameWithHandle() {
+function NameWithHandle({author}) {
+    const {name, handle} = author;
     return (
         <span className={'name-with-handle'}>
-            <span className={'name'}>Your Name</span>
-            <span className={'name'}>@yourhandle</span>
+            <span className={'name'}>{name}</span>
+            <span className={'name'}>@{handle}</span>
         </span>
     )
 }
 
-const Time = () => (
-    <span className='time'>3h ago</span>
-)
+const Time = ({time}) => {
+    const timeString = Moment(time).fromNow();
+    return (
+        <span className='time'>{timeString}</span>
+    );
+}
 
-const LikeButton = () => {
-    return <i className='fa fa-heart like-button'/>
+const LikeButton = ({count}) => {
+    return <span className={'like-button'}>
+        <i className='fa fa-heart'/>
+        {count > 0 && <span className={'like-count'}></span>}
+    </span>
+
 }
 
 
@@ -104,8 +127,21 @@ const CommentButton = () => (
     <i className={'far fa-comment'}/>
 )
 
-const RetweetButton = () => (
-    <i className={'fa fa-retweet retweet-button'}/>
+function getRetweetCount(count) {
+    if (count > 0) {
+        return (
+            <span className={"retweet-count"}>{count}</span>
+        );
+    } else {
+        return null;
+    }
+}
+
+const RetweetButton = ({count}) => (
+    <span className={'retweet-button'}>
+        <i className={'fa fa-retweet'}/>
+        {getRetweetCount(count)}
+    </span>
 )
 
 const ShareButton = () => {
@@ -134,6 +170,37 @@ const Hello = ({fName, lName}) => {
 
 const Person = props => <h1>{props.name + " " + props.age}</h1>
 
-ReactDom.render(<Hello fName={"hung"} lName={"ly"}></Hello>, document.querySelector('#root'));
+class Parent extends React.Component{
+    state = {
+        actionCount: 0
+    }
+    handleAction = (action) => {
+        console.log('Child says', action);
+        this.setState({
+            actionCount: this.state.actionCount + 1
+        })
+    }
 
-// ReactDom.render(<Tweet/>, document.querySelector('#root'));
+    render() {
+        return (
+            <div>
+                <Child onAction={this.handleAction()}/>
+                <p>Clicked {this.state.actionCount} times</p>
+            </div>
+        )
+    }
+}
+
+function Child({onAction}) {
+    return (
+        <button onClick={onAction}>Click Me!</button>
+    )
+}
+
+
+
+// ReactDom.render(<Hello fName={"hung"} lName={"ly"}></Hello>, document.querySelector('#root'));
+
+// ReactDom.render(<Tweet tweet={testTweet}/>, document.querySelector('#root'));
+
+ReactDom.render(<Parent/>, document.querySelector('#root'));
